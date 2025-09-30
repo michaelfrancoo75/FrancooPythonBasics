495865a8c03153f6e64bf3b27be434ba7cdbb470
Michael Francoo <michaelfrancooedu@gmail.com>
2025-09-30 14:25:06 +0300

    add variables (name, age, country) and print sentence

--- homework1.py
+++ homework1.py
@@ -5,3 +5,9 @@ print("My dream is to become an AI Engineer")
 # Comments Example
 # This variable stores my name as a string
 name = "Michael"
+
+# Variables
+name = "Michael"
+age = 19
+country = "Tanzania"
+print(f"My name is {name}, I am {age} years old and I live in {country}")
